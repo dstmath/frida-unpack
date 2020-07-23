e10503fceed2cc1a669a496b720d3c0b5b49bdc8
toor <dstmath@163.com>
2020-07-23 16:28:23 +0800

    适配python3＆android 10

--- frida_unpack.py
+++ frida_unpack.py
@@ -6,7 +6,7 @@ import sys
 def on_message(message, data):
     base = message['payload']['base']
     size = int(message['payload']['size'])
-    print hex(base),size
+    print(hex(base),size)
     # print session
     # dex_bytes = session.read_bytes(base, size)
     # f = open("1.dex","wb")
@@ -16,13 +16,16 @@ def on_message(message, data):
 # 9.0 arm 需要拦截　_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE
 # 7.0 arm：_ZN3art7DexFile10OpenMemoryEPKhjRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPNS_6MemMapEPKNS_10OatDexFileEPS9_
 
+# android 10: libdexfile.so 
+# #_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE
+
 package = sys.argv[1]
-print "dex 导出目录为: /data/data/%s"%(package)
+print("dex 导出目录为: /data/data/%s"%(package))
 device = frida.get_usb_device()
 pid = device.spawn(package)
 session = device.attach(pid)
 src = """
-Interceptor.attach(Module.findExportByName("libart.so", "_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE"), {
+Interceptor.attach(Module.findExportByName("libdexfile.so", "_ZN3art13DexFileLoader10OpenCommonEPKhjS2_jRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE"), {
     onEnter: function (args) {
       
         var begin = args[1]
